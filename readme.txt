0c4b23291a5f667d55aa125892a75c0655fb5ca6
techpriester <techpriester@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-15 19:13:22 +0000

    Added version 1.2.0. See changelog in readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/varnish-http-purge/trunk@410411 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Leon Weidauer
 Tags: varnish, purge, cache
 Requires at least: 1.0.0
 Tested up to: 3.2.1
-Stable tag: 1.1.1
+Stable tag: 1.2.0
 
 == Description ==
 Plugin for invalidating Wordpress items on a Varnish 3 Cache.
@@ -16,7 +16,13 @@ In order to work, the varnish cache meeds to accept PURGE request from the host 
 Tested with Varnish 3.x
 
 == Installation ==
-Download or git-clone into your plugin directory.
+Download or git-clone into your plugin directory. Or simply install via Wordpress admin interface.
 Activate.
 Done. No Configuration needed.
+
+== Changelog ==
+
+= 1.2.0 =
+* Moved actual request execution to "shutdown" event
+* Removed GET request due to bad performance impact
 
--- varnish-http-purge.php
+++ varnish-http-purge.php
@@ -3,7 +3,7 @@
 Plugin Name: Varnish HTTP Purge
 Plugin URI: http://wordpress.org/extend/plugins/varnish-http-purge/ 
 Description: Sends HTTP PURGE requests to URLs of changed posts/pages when they are modified. Works with Varnish 3.
-Version: 1.1.1
+Version: 1.2.0
 Author: Leon Weidauer
 Author URI: http:/sevenmil.es/
 License: Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
@@ -11,7 +11,7 @@ License: Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
 
 class VarnishPurger
 {
-    protected $purgedUrls = array();
+    protected $purgeUrls = array();
     
     public function __construct()
     {
@@ -19,6 +19,8 @@ class VarnishPurger
         {
             add_action($event, array($this, 'purgePost'));
         }
+
+        add_action('shutdown', array($this, 'executePurge'));
     }
 
     protected function getRegisterEvents()
@@ -27,38 +29,31 @@ class VarnishPurger
             'publish_post',
             'edit_post',
             'deleted_post',
+            'save_post',
         );
+    }
+
+    public function executePurge()
+    {
+        $purgeUrls = array_unique($this->purgeUrls);
+
+        foreach($purgeUrls as $url)
+        {
+            $this->purgeUrl($url);
+        }
     }
 
     protected function purgeUrl($url)
     {
-        if (in_array($url, $this->purgedUrls))
-        {
-            //Don't purge the same item more than once in one run.
-            return;
-        }
-
         $c = curl_init($url);
         curl_setopt($c, CURLOPT_CUSTOMREQUEST, 'PURGE');
         curl_exec($c);
         curl_close($c);    
-        
-        array_push($this->purgedUrls, $url);
-
-        $this->callUrl($url);
     }
 
-    protected function callUrl($url)
-    {
-        $c = curl_init($url);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-        curl_exec($c);
-        curl_close($c);    
-    }
-    
     public function purgePost($postId)
     {
-        $this->purgeUrl(get_permalink($postId));
+        array_push($this->purgeUrls, get_permalink($postId));
     }
 }
 
